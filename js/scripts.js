546a1393ec38d1b08c9df88b270a61de4f5eecf5
Kristen Kulha and snow vilay <snow@>
2017-08-15 15:57:10 -0700

    add javascript but still not functionally

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
+  $("#prime-form").submit(function(event){
+    event.preventDefault();
+    var numberInput = parseInt($("#number").val());
+    var arrayNumbers = [];
+    var prime = 2;
+    var index = 0;
 
+    for(var i = numberInput; i >= 2; i --){
+        arrayNumbers.push(i);
+    }
+
+    while(prime != numberInput){
+      arrayNumbers.forEach(function(number){
+        index ++;
+        if((number % prime) == 0) {
+          arrayNumbers.splice(index);
+        }
+      });
+    }
+  }
+    // arrayNumbers.forEach(function(number){
+    //   if((number % prime) == 0){
+    //     arrayNumbers.splice(index);
+    //     index++;
+    //
+    //   }else{
+    //     index++;
+    //   }
+    // });
+    console.log(arrayNumbers);
+  });
 });
